477c4a80210c2e9145a7fb67e39e320d9feb5f62
jonpena <gran_200014@hotmail.com>
2025-01-12 03:22:51 +0100

    Updates

---
+++ main.js
@@ -0,0 +1,14 @@
+var Module = {
+  canvas: document.getElementById("canvas"),
+  onRuntimeInitialized: function () {
+    console.log("WASM cargado y listo!");
+
+    document.getElementById("pause").addEventListener("change", function () {
+      Module._setPause(this.checked);
+    });
+
+    document.getElementById("options").addEventListener("change", function () {
+      Module._setCorrectionType(this.value);
+    });
+  },
+};
