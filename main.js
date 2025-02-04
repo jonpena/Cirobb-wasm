6a65d7c745ced08fdd5239042de8fa006438a7c2
jonpena <gran_200014@hotmail.com>
2025-02-04 06:29:09 +0100

    Updates

--- main.js
+++ main.js
@@ -2,12 +2,17 @@ var Module = {
   canvas: document.getElementById("canvas"),
   onRuntimeInitialized: function () {
     console.log("WASM cargado y listo!");
+
     document.getElementById("pause").addEventListener("change", function () {
       Module._setPause(this.checked);
     });
 
     document.getElementById("options").addEventListener("change", function () {
       Module._setCorrectionType(this.value);
+    });
+
+    document.getElementById("canvas").addEventListener("click", function (e) {
+      Module._setTouchDevice(true);
     });
   },
 };
